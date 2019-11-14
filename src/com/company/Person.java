da8523b92f5b1b4f950ca791df30dee2f7436e70
Diego Strubolini <diego.strubolini@mulesoft.com>
2019-11-14 17:52:05 -0300

    Agregue last name

--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -3,12 +3,18 @@ package com.company;
 public class Person {
 
     private String name;
+    private String lastName;
 
-    public Person(String name) {
+    public Person(String name, String lastName) {
         this.name = name;
+        this.lastName = lastName;
     }
 
     public String getName() {
         return name;
+    }
+
+    public String getLastName() {
+        return lastName;
     }
 }
