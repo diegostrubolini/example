212053d2391237cd13fc710af7a9ddc43acc251f
Diego Strubolini <diego.strubolini@mulesoft.com>
2019-11-14 17:33:11 -0300

    Agregue getName y toString

--- src/com/company/User.java
+++ src/com/company/User.java
@@ -1,4 +1,13 @@
 package com.company;
 
 public class User {
+
+    public String getName() {
+        return "Diego";
+    }
+
+    @Override
+    public String toString() {
+        return "A User";
+    }
 }
